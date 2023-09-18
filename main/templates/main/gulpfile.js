69d313ef6940a7c77d7d503cf1518efad9d04774
semiolujobi <semi_olujobi1@baylor.edu>
2023-09-18 12:43:59 -0500

    changes 3

--- main/templates/main/gulpfile.js
+++ main/templates/main/gulpfile.js
@@ -1,1 +1,10 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
 exports.build = series(scssTask, jsTask);
